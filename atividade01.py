43b75e435bb3645218d909b2bff00d6c8fe1ca56
rodrigosouza <mc2016.rodrigo@gmail.com>
2025-04-03 16:11:59 -0300

    adicionando atividade01

---
+++ atividade01.py
@@ -0,0 +1,11 @@
+hora1 = int(input("qual a hora do nascimento do seu filgo:    "))
+minuto1 = int(input("qual o minuto:    "))
+hora2 = int(input("qual a hora do nascimento do seu pai:    "))
+minuto2 = int(input("qual o minuto:    "))
+
+
+horaFilho = (hora1+minuto1)
+horaPai = (hora2+minuto2)//12
+
+
+if hora
